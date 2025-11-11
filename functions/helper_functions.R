7eed0a2a9783dd8de643b7dc27cb67f475725cf4
Lennert <47691105+LJGroot@users.noreply.github.com>
2025-11-11 13:38:07 +0100

    Source multiple helper functions from external scripts
    added comments describing the components

--- functions/helper_functions.R
+++ functions/helper_functions.R
@@ -1,4 +1,13 @@
+# # Source all helper functions in this folder at once
+
+# adding dummy variables
 source("https://raw.githubusercontent.com/LJGroot/parmod/refs/heads/main/functions/add_dummies.R")
+
+# Factor structure mask
 source("https://raw.githubusercontent.com/LJGroot/parmod/refs/heads/main/functions/factor_mask.R")
+
+# make mxAlgebra objects from string, a) linear, b) log-linear, and c) fisher's Z transformed
 source("https://raw.githubusercontent.com/LJGroot/parmod/refs/heads/main/functions/make_alg.R")
+
+# make final matrix objects for factor model with configural invariance (fully moderated)
 source("https://raw.githubusercontent.com/LJGroot/parmod/refs/heads/main/functions/make_matrices_config.R")
